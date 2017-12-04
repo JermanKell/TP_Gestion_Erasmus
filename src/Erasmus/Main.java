cfc9e909c1aef038a8f73e94128be4269fb3fdcf
JermanKell <xbouchenard@gmail.com>
2017-12-04 11:58:41 +0100

    Rectif pbs

--- src/Erasmus/Main.java
+++ src/Erasmus/Main.java
@@ -1,10 +1,14 @@
 package Erasmus;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("Erasmus");
+		
 	}
 
 }
